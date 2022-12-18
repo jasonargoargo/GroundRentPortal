520384397d2241cafaf6e76bffd929239d44fe74
Aidan Gignac <aidangig@gmail.com>
2022-12-18 11:04:30 -0800

    Created Dark Mode Cookie Set Logic
    Created JavaScript cookie to also handle proper logic of isDarkModeCookie and toggling logic automatically per cookie value.

--- UI/wwwroot/js/cookie.js
+++ UI/wwwroot/js/cookie.js
@@ -3,7 +3,16 @@
     console.log(`cookie set as ${themeName}`);
 }
 
+function setDarkModeCookie(isDarkMode) {
+    document.cookie = `isDarkMode=${isDarkMode}`;
+    console.log(`isDarkMode cookie set as ${isDarkMode}`);  
+}
+
 function getTheme() {
+    console.log(`cookie returned as ${document.cookie}`);
+    return document.cookie;
+}
+function getDarkThemeCookie() {
     console.log(`cookie returned as ${document.cookie}`);
     return document.cookie;
 }
