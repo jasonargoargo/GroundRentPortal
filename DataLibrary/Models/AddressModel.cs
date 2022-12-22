5354e877cd60d0af3833fe8314cd216987ff47d4
Aidan Gignac <aidangig@gmail.com>
2022-12-22 06:39:33 -0800

    Updated Validation Logic for NotLegibleType
    Updated validation logic to compensate for Required attribute checking NotLegible value selected. Allowed passthrough of logic to allow form submission to submit through case scenarios as needed.

--- DataLibrary/Models/AddressModel.cs
+++ DataLibrary/Models/AddressModel.cs
@@ -23,22 +23,22 @@ namespace DataLibrary.Models
 		[RequiredIf("IsLegible == false", ErrorMessage = "Input what is illegible!")]
         public InputType? NotLegibleType { get; set; }
         [MaxLength(2048)] public string? DataDifferencesJson { get; set; }
-        [Required] [DataType(DataType.Currency)] public decimal? PaymentAmount { get; set; }
+        [RequiredIf("NotLegibleType != AddressModel.InputType.PaymentAmount")] [DataType(DataType.Currency)] public decimal? PaymentAmount { get; set; }
         public enum PaymentFrequency { Annual, SemiAnnual, Quarterly, Other }
         [Required] public PaymentFrequency? GroundRentPaymentFrequency { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == PaymentFrequency.Annual", ErrorMessage = "An annual date selection must be made")]
+		[RequiredIf("GroundRentPaymentFrequency == PaymentFrequency.Annual && NotLegibleType != AddressModel.InputType.PaymentDateAnnual", ErrorMessage = "An annual date selection must be made")]
         [DataType(DataType.DateTime)] public DateTime? PaymentDateAnnual { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.SemiAnnual", ErrorMessage = "All Semi-Annual date selections must be made.")]
+		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.SemiAnnual && NotLegibleType != AddressModel.InputType.PaymentDateSemiAnnual1", ErrorMessage = "All Semi-Annual date selections must be made.")]
 		[DataType(DataType.DateTime)] public DateTime? PaymentDateSemiAnnual1 { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.SemiAnnual", ErrorMessage = "All Semi-Annual date selections must be made.")]
+		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.SemiAnnual && NotLegibleType != AddressModel.InputType.PaymentDateSemiAnnual2", ErrorMessage = "All Semi-Annual date selections must be made.")]
 		[DataType(DataType.DateTime)] public DateTime? PaymentDateSemiAnnual2 { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly", ErrorMessage = "All Quarterly date selections must be made.")]
+		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly && NotLegibleType != AddressModel.InputType.PaymentDateQuarterly1", ErrorMessage = "All Quarterly date selections must be made.")]
 		[DataType(DataType.DateTime)] public DateTime? PaymentDateQuarterly1 { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly", ErrorMessage = "All Quarterly date selections must be made.")]
+		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly && NotLegibleType != AddressModel.InputType.PaymentDateQuarterly2", ErrorMessage = "All Quarterly date selections must be made.")]
 		[DataType(DataType.DateTime)] public DateTime? PaymentDateQuarterly2 { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly", ErrorMessage = "All Quarterly date selections must be made.")]
+		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly && NotLegibleType != AddressModel.InputType.PaymentDateQuarterly3", ErrorMessage = "All Quarterly date selections must be made.")]
 		[DataType(DataType.DateTime)] public DateTime? PaymentDateQuarterly3 { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly", ErrorMessage = "All Quarterly date selection must be made.")]
+		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly && NotLegibleType != AddressModel.InputType.PaymentDateQuarterly4", ErrorMessage = "All Quarterly date selection must be made.")]
 		[DataType(DataType.DateTime)] public DateTime? PaymentDateQuarterly4 { get; set; }
 		[MaxLength(64)]public string? PaymentDateOther { get; set; }
         public string? UserWhoProcessed { get; set; }
