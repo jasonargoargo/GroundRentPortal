f321c47828ac96475a4ea1d9138cdbb124b26d07
Aidan Gignac <aidangig@gmail.com>
2022-12-22 08:24:32 -0800

    Further Improved Razor & Validation Logic
    Improved various validation errors and output information to give the user context on what is needed and required to allow a form submission. Adjusted various Razor markup to successfully flow UI View logic properly.

--- DataLibrary/Models/AddressModel.cs
+++ DataLibrary/Models/AddressModel.cs
@@ -20,27 +20,28 @@ namespace DataLibrary.Models
         public bool? IsVerified { get; set; }
 		[Required] public bool? IsLegible { get; set; }		
 		public enum InputType { Blank, PaymentAmount, PaymentDateAnnual, PaymentDateSemiAnnual1, PaymentDateSemiAnnual2, PaymentDateQuarterly1, PaymentDateQuarterly2, PaymentDateQuarterly3, PaymentDateQuarterly4, PaymentDateOther }
-		[RequiredIf("IsLegible == false", ErrorMessage = "Input what is illegible!")]
+		[RequiredIf("IsLegible == false", ErrorMessage = "Illegible field is required!")]
         public InputType? NotLegibleType { get; set; }
         [MaxLength(2048)] public string? DataDifferencesJson { get; set; }
-        [RequiredIf("NotLegibleType != AddressModel.InputType.PaymentAmount")] [DataType(DataType.Currency)] public decimal? PaymentAmount { get; set; }
+        [RequiredIf("NotLegibleType != AddressModel.InputType.PaymentAmount", ErrorMessage = "Payment Amount field is required.")] [DataType(DataType.Currency)] public decimal? PaymentAmount { get; set; }
         public enum PaymentFrequency { Annual, SemiAnnual, Quarterly, Other }
         [Required] public PaymentFrequency? GroundRentPaymentFrequency { get; set; }
 		[RequiredIf("GroundRentPaymentFrequency == PaymentFrequency.Annual && NotLegibleType != AddressModel.InputType.PaymentDateAnnual", ErrorMessage = "An annual date selection must be made")]
         [DataType(DataType.DateTime)] public DateTime? PaymentDateAnnual { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.SemiAnnual && NotLegibleType != AddressModel.InputType.PaymentDateSemiAnnual1", ErrorMessage = "All Semi-Annual date selections must be made.")]
+		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.SemiAnnual && NotLegibleType != AddressModel.InputType.PaymentDateSemiAnnual1", ErrorMessage = "A Semi-Annual date selection must be made.")]
 		[DataType(DataType.DateTime)] public DateTime? PaymentDateSemiAnnual1 { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.SemiAnnual && NotLegibleType != AddressModel.InputType.PaymentDateSemiAnnual2", ErrorMessage = "All Semi-Annual date selections must be made.")]
+		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.SemiAnnual && NotLegibleType != AddressModel.InputType.PaymentDateSemiAnnual2", ErrorMessage = "A Semi-Annual date selection must be made.")]
 		[DataType(DataType.DateTime)] public DateTime? PaymentDateSemiAnnual2 { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly && NotLegibleType != AddressModel.InputType.PaymentDateQuarterly1", ErrorMessage = "All Quarterly date selections must be made.")]
+		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly && NotLegibleType != AddressModel.InputType.PaymentDateQuarterly1", ErrorMessage = "A Quarterly date selection must be made.")]
 		[DataType(DataType.DateTime)] public DateTime? PaymentDateQuarterly1 { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly && NotLegibleType != AddressModel.InputType.PaymentDateQuarterly2", ErrorMessage = "All Quarterly date selections must be made.")]
+		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly && NotLegibleType != AddressModel.InputType.PaymentDateQuarterly2", ErrorMessage = "A Quarterly date selection must be made.")]
 		[DataType(DataType.DateTime)] public DateTime? PaymentDateQuarterly2 { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly && NotLegibleType != AddressModel.InputType.PaymentDateQuarterly3", ErrorMessage = "All Quarterly date selections must be made.")]
+		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly && NotLegibleType != AddressModel.InputType.PaymentDateQuarterly3", ErrorMessage = "A Quarterly date selection must be made.")]
 		[DataType(DataType.DateTime)] public DateTime? PaymentDateQuarterly3 { get; set; }
-		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly && NotLegibleType != AddressModel.InputType.PaymentDateQuarterly4", ErrorMessage = "All Quarterly date selection must be made.")]
+		[RequiredIf("GroundRentPaymentFrequency == AddressModel.PaymentFrequency.Quarterly && NotLegibleType != AddressModel.InputType.PaymentDateQuarterly4", ErrorMessage = "A Quarterly date selection must be made.")]
 		[DataType(DataType.DateTime)] public DateTime? PaymentDateQuarterly4 { get; set; }
-		[MaxLength(64)]public string? PaymentDateOther { get; set; }
+        [RequiredIf("NotLegibleType != AddressModel.InputType.PaymentDateOther", ErrorMessage = "Other input field is required!")]
+        [MaxLength(64)]public string? PaymentDateOther { get; set; }
         public string? UserWhoProcessed { get; set; }
         public string? UserWhoVerified { get; set; }
     }
